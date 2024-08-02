32331a96e8b135a17970c48c03cd79c1cf33c24d
carolinekle <caroline.klewinowski@gmail.com>
2024-08-02 13:35:44 -0400

    add cms app

--- capstone/settings.py
+++ capstone/settings.py
@@ -41,7 +41,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "tinymce"
 ]
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
--- news/urls.py
+++ news/urls.py
@@ -1,4 +1,4 @@
-from django.urls  import path
+from django.urls  import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
@@ -6,6 +6,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="homepage"),
+    path('tinymce/', include('tinymce.urls')),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
